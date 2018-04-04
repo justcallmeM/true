48034890cc623ca6b090a3e04ec1a395fe07a197
justcallmeM <36482664+justcallmeM@users.noreply.github.com>
2018-04-04 10:40:54 +0300

    Refresh button & navigation

--- app/src/main/java/com/example/dinopc/atsimink/MainActivity.java
+++ app/src/main/java/com/example/dinopc/atsimink/MainActivity.java
@@ -1,10 +1,19 @@
 package com.example.dinopc.atsimink;
 
+import android.Manifest;
 import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.databinding.DataBindingUtil;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.annotation.MainThread;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -12,10 +21,12 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.dinopc.atsimink.Settings.AboutUs;
-import com.example.dinopc.atsimink.WeekDays.Current;
 import com.example.dinopc.atsimink.WeekDays.Friday;
 import com.example.dinopc.atsimink.WeekDays.Monday;
 import com.example.dinopc.atsimink.WeekDays.Saturday;
@@ -27,25 +38,85 @@ import com.example.dinopc.atsimink.WeekDays.Wednesday;
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
+    private Button button;
+    private TextView textView;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mDrawerLayout=(DrawerLayout) findViewById(R.id.drawer);
-        mToggle=new ActionBarDrawerToggle(this, mDrawerLayout,R.string.open,R.string.close);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        NavigationView navigationView=(NavigationView)findViewById(R.id.Navigation_v);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.Navigation_v);
         navigationView.setNavigationItemSelectedListener(this);
 
-        Current current = new Current();
-        android.support.v4.app.FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.replace(R.id.fragment_container, current);
-        fragmentTransaction.commit();
+        //GPS
+        button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView);
+        //Inititalize location manager & listener
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                textView.append("\n"+location.getLatitude() +" "+location.getLongitude());
+            }
 
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+
+            }
+        };
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{
+                        Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.INTERNET
+                }, 10 );
+                return;
+            }
+        }else{
+            configureButton();
+        }
+        locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 10:
+                if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    configureButton();
+                return;
+        }
+    }
+
+    private void configureButton() {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //REQUESTION LOCATION AND UPDATING AFTER 5SECONDS (time = 5000)
+                locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
+            }
+        });
 
     }
 
@@ -57,9 +128,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     }
 
+    //SEARCH AND SETTINGS MENU
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        //SEARCH AND SETTINGS MENU
         switch(item.getItemId()) {
             case R.id.action_refresh:
 
@@ -93,17 +164,16 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         return super.onOptionsItemSelected(item);
     }
 
+    //DRAWER LAYOUT
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
         int id = item.getItemId();
 
-        if(id == R.id.Current)
+        if(id == R.id.Main)
         {
-            Current current = new Current();
-            android.support.v4.app.FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-            fragmentTransaction.replace(R.id.fragment_container, current);
-            fragmentTransaction.commit();
+            Intent i = new Intent(MainActivity.this, MainActivity.class);
+            startActivity(i);
             mDrawerLayout.closeDrawers();
         }
         else if(id == R.id.Monday)
@@ -165,4 +235,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         return true;
     }
+
+    //
 }
